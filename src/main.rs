85327be25c586fe78cba9cb659f162a093b9dd9a
Thomas Haller <thomashaller@gmx.at>
2025-05-10 19:24:03 +0200

    experimental changes for web builds

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 use atlas_test::run_atlas_test;
 use bevy::app::App;
 use crossequa_plugin::CrossequaPlugin;
+use lighting::main_lighting;
 
 mod atlas_test;
 mod crossequa_plugin;
 mod gameboard;
 mod grid;
 mod texture_manager;
+mod lighting;
 
 fn main() {
     println!("Hello, world!");
 
     //run_atlas_test();
 
-    App::new().add_plugins(CrossequaPlugin).run();
+    main_lighting();
+
+    //App::new().add_plugins(CrossequaPlugin).run();
 }
