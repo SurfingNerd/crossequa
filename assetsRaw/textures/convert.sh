d7d1e6d22d582bf5fb15f4c826fc7f0bf4dc428b
Thomas Haller <thomashaller@gmx.at>
2025-05-11 14:22:48 +0200

    assets holds the assets that get included, and assetsRaw the source of it.

---
+++ assetsRaw/textures/convert.sh
@@ -0,0 +1,14 @@
+
+IMGID="03"
+POSITION="1024x820+1024+412"
+ASSET_NAME="screen_"
+
+
+convert Scifi_Panels_${IMGID}_basecolor.png -crop $POSITION cropped/${ASSET_NAME}basecolor.png
+convert Scifi_Panels_${IMGID}_emissive.png -crop $POSITION cropped/${ASSET_NAME}emissive.png
+convert Scifi_Panels_${IMGID}_emissiveMask.png -crop $POSITION cropped/${ASSET_NAME}emissiveMask.png
+convert Scifi_Panels_${IMGID}_height.png -crop $POSITION cropped/${ASSET_NAME}height.png
+convert Scifi_Panels_${IMGID}_roughness.png -crop $POSITION cropped/${ASSET_NAME}roughness.png
+convert Scifi_Panels_${IMGID}_normal.png -crop $POSITION cropped/${ASSET_NAME}normal.png
+convert Scifi_Panels_${IMGID}_basecolor.png -crop $POSITION cropped/${ASSET_NAME}basecolor.png
+convert Scifi_Panels_${IMGID}_basecolor.png -crop $POSITION cropped/${ASSET_NAME}basecolor.png
