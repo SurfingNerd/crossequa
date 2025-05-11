189faf1ff4345389c3892a7c3573d3fad8b0ec0d
Thomas Haller <thomashaller@gmx.at>
2025-05-11 14:43:04 +0200

    texture manager for new assets

--- src/texture_manager.rs
+++ src/texture_manager.rs
@@ -11,20 +11,24 @@ pub struct TextureManager {
 
 impl TextureManager {
     pub fn new(asset_server: &AssetServer) -> Self {
-        let base_color_texture = asset_server.load("textures/Scifi_Panels_01_basecolor.png");
-        let metallic_roughtness_texture =
-            asset_server.load("textures/Scifi_Panels_01_roughness.png");
-        let emissive_texture = asset_server.load("textures/Scifi_Panels_01_emissive.png");
-        let occlusion_texture = asset_server.load("textures/Scifi_Panels_01_ambientocclusion.png");
-        let normal_map_texture = asset_server.load("textures/Scifi_Panels_01_normal.png");
+        
+        let asset_name = "screen";
+        
+        let occlusion_texture = asset_server.load(format!("textures/{}_ambientocclusion.png", asset_name ));
+        let base_color_texture = asset_server.load(format!("textures/{}_basecolor.png", asset_name ));
+        let metallic_roughness_texture = asset_server.load(format!("textures/{}roughness.png", asset_name ));
+        let emissive_texture = asset_server.load(format!("textures/{}_emissive.png", asset_name ));
+        let normal_map_texture = asset_server.load(format!("textures/{}_normal.png", asset_name ));
+
 
         let tile_material = StandardMaterial {
             // vary key PBR parameters on a grid of spheres to show the effect
             base_color_texture: Some(base_color_texture.clone()),
-            metallic_roughness_texture: Some(metallic_roughtness_texture.clone()),
+            metallic_roughness_texture: Some(metallic_roughness_texture.clone()),
             emissive_texture: Some(emissive_texture.clone()),
             occlusion_texture: Some(occlusion_texture.clone()),
             normal_map_texture: Some(normal_map_texture.clone()),
+            
             metallic: 0.5,
             perceptual_roughness: 0.5,
             ..default()
